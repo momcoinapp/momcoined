be4493fd43f037866af2441349690054c8e92d6c
MomCoin Dev <mom@momcoined.com>
2025-12-05 09:00:43 -0500

    Fix build errors by ignoring pino-pretty and fs modules

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
   },
   transpilePackages: ['@farcaster/auth-kit', '@coinbase/onchainkit'],
   // reactCompiler: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 export default nextConfig;
