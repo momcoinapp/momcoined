cf5b5f328cc63d87e062725fa95aa52b0542e464
MomCoin Dev <mom@momcoined.com>
2025-12-07 13:29:39 -0500

    Fix syntax error in FarcasterProvider config

--- components/auth/FarcasterProvider.tsx
+++ components/auth/FarcasterProvider.tsx
@@ -6,18 +6,22 @@ import { useEffect } from "react";
 import sdk from "@farcaster/miniapp-sdk";
 
 const config = {
+    rpcUrl: "https://mainnet.optimism.io",
+    domain: "momcoined.com",
+    siweUri: "https://momcoined.com/login",
+};
 
-    export default function FarcasterProvider({ children }: { children: React.ReactNode }) {
-        useEffect(() => {
-            const load = async () => {
-                sdk.actions.ready();
-            };
-            load();
-        }, []);
+export default function FarcasterProvider({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const load = async () => {
+            sdk.actions.ready();
+        };
+        load();
+    }, []);
 
-return (
-    <AuthKitProvider config={config}>
-        {children}
-    </AuthKitProvider>
-);
+    return (
+        <AuthKitProvider config={config}>
+            {children}
+        </AuthKitProvider>
+    );
 }
